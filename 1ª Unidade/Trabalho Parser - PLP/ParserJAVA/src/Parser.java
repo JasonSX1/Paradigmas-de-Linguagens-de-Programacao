31d9d4e4a7b7d1bc58061fd25a4e8f5b84c56077
Geison de Oliveira <geisonyt3@gmail.com>
2024-10-28 08:37:02 -0300

    -

---
+++ 1ª Unidade/Trabalho Parser - PLP/ParserJAVA/src/Parser.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Parser {
+    public static void main(String[] args) {
+        String pathNotebook = "C:\\Users\\Cliente\\Desktop\\IV Semestre\\Paradigmas-de-Linguagens-de-Programacao\\Trabalho Parser - PLP\\ParserJAVA\\src\\Programa1.plp";
+        String pathPC = "C:\\Users\\geiso\\Desktop\\IV Semestre\\Paradigmas-de-Linguagens-de-Programacao\\Trabalho Parser - PLP\\ParserJAVA\\src\\Programa1.plp";
+
+        String nomeArquivo = pathPC; // Path do arquivo de código-fonte
+    
+        StringBuilder entrada = new StringBuilder();
+    
+        try (BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                entrada.append(linha).append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+            return;
+        }
+    
+        Transicoes transicoes = new Transicoes();
+        boolean resultado = transicoes.processar(entrada.toString());
+        System.out.println("Entrada " + (resultado ? "válida" : "inválida"));
+    }
+}
