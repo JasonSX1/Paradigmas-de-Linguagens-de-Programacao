490f123f8e0ac9c6cb9ac2c305ae0cf7d3b92468
Geison de Oliveira <geisonyt3@gmail.com>
2024-12-13 13:42:22 -0300

    Update t2q6.scala

--- 2ª Unidade/Prova/src/main/scala/RevisaoProva/t2q6.scala
+++ 2ª Unidade/Prova/src/main/scala/RevisaoProva/t2q6.scala
@@ -3,5 +3,8 @@ object maint2q6 extends App {
     lista => lista.forall(isValid)
   }
 
-  
+  val par: (Int => Boolean) = x => x%2 == 0
+  val maiorQue25: (Int => Boolean) = x => x>25
+  val positivo: (Int => Boolean) = x => x>25
+
 }
