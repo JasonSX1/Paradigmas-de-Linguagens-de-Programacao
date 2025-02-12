fcec2ec198e972084f3c66b1ec5022a64809af1e
Geison de Oliveira <geisonyt3@gmail.com>
2025-02-12 09:10:33 -0300

    Ordenação dos fatos e regras e Correção na consulta

--- 3ª Unidade/ProjetoClassifAnimais/fatos_regras.pl
+++ 3ª Unidade/ProjetoClassifAnimais/fatos_regras.pl
@@ -1,43 +1,45 @@
 % Fatos sobre animais
-animal(urso, [habitat(floresta), comportamento(solitario), dieta(onivoro)]).
-animal(gato, [habitat(urbano), comportamento(independente), dieta(carnivoro)]).
-animal(peixe, [habitat(aquatico), comportamento(cardume), dieta(herbivoro)]).
 animal(aguia, [habitat(montanha), comportamento(solitario), dieta(carnivoro)]).
-animal(elefante, [habitat(savana), comportamento(social), dieta(herbivoro)]).
-animal(leao, [habitat(savana), comportamento(social), dieta(carnivoro)]).
-animal(lobo, [habitat(floresta), comportamento(em_bando), dieta(carnivoro)]).
+animal(antilope, [habitat(savana), comportamento(em_bando), dieta(herbivoro)]).
+animal(arara, [habitat(amazonia), comportamento(social), dieta(frugivoro)]).
+animal(camelo, [habitat(deserto), comportamento(em_grupo), dieta(herbivoro)]).
+animal(canguru, [habitat(australia), comportamento(em_grupo), dieta(herbivoro)]).
 animal(cavalo, [habitat(campo), comportamento(social), dieta(herbivoro)]).
+animal(cervo, [habitat(floresta), comportamento(em_grupo), dieta(herbivoro)]).
 animal(cobra, [habitat(floresta), comportamento(solitario), dieta(carnivoro)]).
 animal(coelho, [habitat(campo), comportamento(em_grupo), dieta(herbivoro)]).
-animal(pinguim, [habitat(polo_sul), comportamento(em_colonia), dieta(carnivoro)]).
-animal(canguru, [habitat(australia), comportamento(em_grupo), dieta(herbivoro)]).
-animal(falacao, [habitat(amazonia), comportamento(social), dieta(frugivoro)]).
-animal(golfinho, [habitat(oceano), comportamento(social), dieta(carnivoro)]).
-animal(tigre, [habitat(floresta), comportamento(solitaria), dieta(carnivoro)]).
-animal(urso_polar, [habitat(polo_norte), comportamento(solitario), dieta(carnivoro)]).
-animal(flamingo, [habitat(pantanal), comportamento(em_grupo), dieta(herbivoro)]).
-animal('jacaré', [habitat(pantanal), comportamento(solitario), dieta(carnivoro)]).
-animal(arara, [habitat(amazonia), comportamento(social), dieta(frugivoro)]).
 animal(coruja, [habitat(floresta), comportamento(solitario), dieta(carnivoro)]).
-animal(cervo, [habitat(floresta), comportamento(em_grupo), dieta(herbivoro)]).
-animal(tubarao, [habitat(oceano), comportamento(solitario), dieta(carnivoro)]).
-animal(ornitorrinco, [habitat(australia), comportamento(solitario), dieta(onivoro)]).
-animal(antilope, [habitat(savana), comportamento(em_bando), dieta(herbivoro)]).
-animal(camelo, [habitat(deserto), comportamento(em_grupo), dieta(herbivoro)]).
-animal(zebra, [habitat(savana), comportamento(em_grupo), dieta(herbivoro)]).
-animal(panda, [habitat(bambu), comportamento(solitario), dieta(herbivoro)]).
-animal(raposa, [habitat(campo), comportamento(solitario), dieta(onivoro)]).
+animal(elefante, [habitat(savana), comportamento(social), dieta(herbivoro)]).
+animal(falacao, [habitat(amazonia), comportamento(social), dieta(frugivoro)]).
+animal(flamingo, [habitat(pantanal), comportamento(em_grupo), dieta(herbivoro)]).
+animal(gato, [habitat(urbano), comportamento(independente), dieta(carnivoro)]).
+animal(golfinho, [habitat(oceano), comportamento(social), dieta(carnivoro)]).
 animal(guaxinim, [habitat(urbano), comportamento(noturno), dieta(onivoro)]).
+animal('jacaré', [habitat(pantanal), comportamento(solitario), dieta(carnivoro)]).
+animal(leao, [habitat(savana), comportamento(social), dieta(carnivoro)]).
+animal(lobo, [habitat(floresta), comportamento(em_bando), dieta(carnivoro)]).
+animal(ornitorrinco, [habitat(australia), comportamento(solitario), dieta(onivoro)]).
+animal(panda, [habitat(bambu), comportamento(solitario), dieta(herbivoro)]).
+animal(peixe, [habitat(aquatico), comportamento(cardume), dieta(herbivoro)]).
+animal(pinguim, [habitat(polo_sul), comportamento(em_colonia), dieta(carnivoro)]).
+animal(raposa, [habitat(campo), comportamento(solitario), dieta(onivoro)]).
 animal(tatu, [habitat(cerrado), comportamento(noturno), dieta(insetivoro)]).
+animal(tigre, [habitat(floresta), comportamento(solitaria), dieta(carnivoro)]).
+animal(tubarao, [habitat(oceano), comportamento(solitario), dieta(carnivoro)]).
+animal(urso, [habitat(floresta), comportamento(solitario), dieta(onivoro)]).
+animal(urso_polar, [habitat(polo_norte), comportamento(solitario), dieta(carnivoro)]).
+animal(zebra, [habitat(savana), comportamento(em_grupo), dieta(herbivoro)]).
+
 
 % Regras para calcular a correspondência de características
 % Conta quantas características fornecidas aparecem na lista de um animal
 contar_correspondencias(CaracteristicasUsuario, CaracteristicasAnimal, N) :-
-    include(
-        (X -> (member(habitat(X), CaracteristicasAnimal);
-               member(comportamento(X), CaracteristicasAnimal);
-               member(dieta(X), CaracteristicasAnimal))),
-        CaracteristicasUsuario, Correspondentes),
+    findall(X, 
+            (member(X, CaracteristicasUsuario), 
+             (member(habitat(X), CaracteristicasAnimal);
+              member(comportamento(X), CaracteristicasAnimal);
+              member(dieta(X), CaracteristicasAnimal))),
+            Correspondentes),
     length(Correspondentes, N).
 
 % Identifica os animais e ordena por probabilidade
